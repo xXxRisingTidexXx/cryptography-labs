bb467380987a17b0142df3970ec44b4031334716
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2020-09-13 13:34:03 +0300

    Added cipher interface

---
+++ cipher.go
@@ -0,0 +1,10 @@
+package cryptolabs
+
+// TODO
+type Cipher interface {
+	// TODO
+	Encrypt(string, string) string
+
+	// TODO
+	Decrypt(string, string) string
+}
--- ciphers_test.go
+++
@@ -1,1 +0,0 @@
-package cryptolabs_test
