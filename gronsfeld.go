de17c884b94063d3805dd6490f028ea70b0b7643
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2020-09-13 14:58:13 +0300

    Added gronsfeld test specs

--- gronsfeld.go
+++ gronsfeld.go
@@ -14,28 +14,28 @@ type gronsfeld struct {
 	alphabet *Alphabet
 }
 
-func (gronsfeld *gronsfeld) Encrypt(message, key string) string {
-	return gronsfeld.transform(message, key, 1)
+func (gronsfeld *gronsfeld) Encrypt(text, key string) string {
+	return gronsfeld.transform(text, key, 1)
 }
 
-func (gronsfeld *gronsfeld) transform(message, key string, direction int) string {
-	if message == "" || key == "" {
+func (gronsfeld *gronsfeld) transform(text, key string, direction int) string {
+	if text == "" || key == "" {
 		return ""
 	}
-	messageRunes, keyRunes := []rune(message), []rune(key)
+	textRunes, keyRunes := []rune(text), []rune(key)
 	shifts := make([]int, len(keyRunes))
 	for i := range keyRunes {
 		if digit := keyRunes[i]; unicode.IsDigit(digit) {
 			shifts[i] = int(digit - '0')
 		}
 	}
-	newRunes := make([]rune, len(messageRunes))
-	for i := range messageRunes {
-		newRunes[i] = gronsfeld.alphabet.Get(messageRunes[i], direction*shifts[i%len(shifts)])
+	newRunes := make([]rune, len(textRunes))
+	for i := range textRunes {
+		newRunes[i] = gronsfeld.alphabet.Get(textRunes[i], direction*shifts[i%len(shifts)])
 	}
 	return string(newRunes)
 }
 
-func (gronsfeld *gronsfeld) Decrypt(message, key string) string {
-	return gronsfeld.transform(message, key, -1)
+func (gronsfeld *gronsfeld) Decrypt(text, key string) string {
+	return gronsfeld.transform(text, key, -1)
 }
---
+++ gronsfeld_test.go
@@ -0,0 +1,11 @@
+package cryptolabs_test
+
+import (
+	"testing"
+)
+
+func TestGronsfeldEncrypt(t *testing.T) {}
+
+func testTexts(t *testing.T, actual, expected string) {}
+
+func TestGronsfeldDecrypt(t *testing.T) {}
