33228b5ee89d84917c27d4d5f9a7ad50a5d2338b
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2020-10-05 00:44:34 +0300

    Implemented feistel

--- feistel.go
+++ feistel.go
@@ -1,7 +1,5 @@
 package cryptolabs
 
-import "fmt"
-
 func NewFeistel(f func(rune, rune, rune) rune) Cipher {
 	return &feistel{f}
 }
@@ -11,29 +9,33 @@ type feistel struct {
 }
 
 func (feistel *feistel) Encrypt(text, key string) string {
-	if len(text) == 0 || len(key) == 0 {
+	if len(text) == 0 || len(key) == 0 || len(text)%2 == 1 {
 		return text
 	}
 	textRunes, keyRunes := []rune(text), []rune(key)
 	newRunes := make([]rune, len(textRunes))
 	for i := 0; i < len(textRunes); i += 2 {
-		left, right := textRunes[i], textRunes[0]
-		isEven := i+1 != len(textRunes)
-		if isEven {
-			right = textRunes[i+1]
-		}
+		left, right := textRunes[i], textRunes[i+1]
 		for j := range keyRunes {
 			left, right = right^feistel.f(rune(j), left, keyRunes[j]), left
 		}
-		newRunes[i] = left
-		if isEven {
-			newRunes[i+1] = right
-		}
+		newRunes[i], newRunes[i+1] = left, right
 	}
-	fmt.Printf("%q\n", newRunes)
 	return string(newRunes)
 }
 
 func (feistel *feistel) Decrypt(text, key string) string {
-	return text
+	if len(text) == 0 || len(key) == 0 || len(text)%2 == 1 {
+		return text
+	}
+	textRunes, keyRunes := []rune(text), []rune(key)
+	newRunes := make([]rune, len(textRunes))
+	for i := 0; i < len(textRunes); i += 2 {
+		left, right := textRunes[i], textRunes[i+1]
+		for j := len(keyRunes) - 1; j >= 0; j-- {
+			left, right = right, left^feistel.f(rune(j), right, keyRunes[j])
+		}
+		newRunes[i], newRunes[i+1] = left, right
+	}
+	return string(newRunes)
 }
