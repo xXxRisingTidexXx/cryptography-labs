c11405fcf24fa2f31a23e65c77278f661bfae6a6
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2020-10-11 21:31:37 +0300

    Finished gost cli

--- cmd/gost2814789/main.go
+++ cmd/gost2814789/main.go
@@ -5,9 +5,11 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/xXxRisingTidexXx/cryptography-labs"
 	"io/ioutil"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	sboxPath := flag.String("s", "examples/gost2814789.sbx", "S-Box file path")
 	textPath := flag.String("t", "examples/gost2814789.txt", "The input message file path")
 	keyPath := flag.String("k", "examples/gost2814789.key", "The key file path")
@@ -18,6 +20,13 @@ func main() {
 	if *isVerbose {
 		log.SetLevel(log.DebugLevel)
 	}
+	sbox, err := ioutil.ReadFile(*sboxPath)
+	if err != nil {
+		log.Fatalf("main: gost2814789 failed to read the sbox, %v", err)
+	}
+	if len(sbox) != 256 {
+		log.Fatalf("main: gost2814789 got sbox with invalid length, %d != 255", len(sbox))
+	}
 	text, err := ioutil.ReadFile(*textPath)
 	if err != nil {
 		log.Fatalf("main: gost2814789 failed to read the text, %v", err)
@@ -26,19 +35,24 @@ func main() {
 	if err != nil {
 		log.Fatalf("main: gost2814789 failed to read the key, %v", err)
 	}
-	textRunes := []rune(string(text))
-	if len(textRunes)%2 == 1 {
-		last := textRunes[len(textRunes)-1]
-		log.Debugf("main: gost2814789 got odd-length message, appended %q to the end", last)
-		textRunes = append(textRunes, last)
+	if modulo := len(text) % 8; modulo != 0 {
+		log.Debug("main: gost2814789 got incomplete message, padded with the last rune")
+		for i := modulo; i < 8; i++ {
+			text = append(text, text[len(text)-1])
+		}
 	}
-	cipher, output := cryptolabs.NewGOST2814789(cryptolabs.NewSBox([8][16]byte{})), ""
+	table := [8][16]byte{}
+	for i := range sbox {
+		table[i/16][i%16] = sbox[i]
+	}
+	cipher, output := cryptolabs.NewGOST2814789(cryptolabs.NewSBox(table)), ""
 	if *isDecryption {
-		output = cipher.Decrypt(string(textRunes), string(key))
+		output = cipher.Decrypt(string(text), string(key))
 	} else {
-		output = cipher.Encrypt(string(textRunes), string(key))
+		output = cipher.Encrypt(string(text), string(key))
 	}
 	if err := ioutil.WriteFile(*outputPath, []byte(output), 0600); err != nil {
 		log.Fatalf("main: gost2814789 failed to write the output, %v", err)
 	}
+	log.Debugf("main: gost2814789 finished in %s", time.Since(start))
 }
