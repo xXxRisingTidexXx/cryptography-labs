eacf23abb24e3b1fc832efd3a138e135d94eb385
xXxRisingTidexXx <danylopetrik@gmail.com>
2020-11-08 19:51:49 +0200

    Checked dss

--- dss.go
+++ dss.go
@@ -1,18 +1,39 @@
 package cryptolabs
 
 func NewDSS(p, q, k, x, g int64) Signer {
-	return &dss{}
+	return &dss{p, q, k, x, g, pow64(g, k, p), mod64(pow64(g, x, p), q)}
+}
+
+func NewExplicitDSS(p, q, g, y, r int64) Signer {
+	return &dss{p: p, q: q, g: g, y: y, r: r}
 }
 
 type dss struct {
-
+	p int64
+	q int64
+	k int64
+	x int64
+	g int64
+	y int64
+	r int64
 }
 
 func (dss *dss) Encode(m, _ int64) int64 {
+	_, i := gcd64(dss.x, dss.q)
+	return mod64(i*mod64(m+dss.k*dss.r, dss.q), dss.q)
+}
+
+func (dss *dss) Decode(_, _ int64) int64 {
 	return 0
 }
 
-func (dss *dss) Decode(s, _ int64) int64 {
-	return 0
+func (dss *dss) Verify(s, m int64) bool {
+	_, w := gcd64(s, dss.q)
+	return dss.r == mod64(
+		mod64(
+			pow64(dss.g, mod64(m*w, dss.q), dss.p)*pow64(dss.y, mod64(dss.r*w, dss.q), dss.p),
+			dss.p,
+		),
+		dss.q,
+	)
 }
-
--- dss_test.go
+++ dss_test.go
@@ -1,9 +1,29 @@
 package cryptolabs_test
 
 import (
+	"github.com/xXxRisingTidexXx/cryptography-labs"
 	"testing"
 )
 
 func TestDSS(t *testing.T) {
-
+	signer, m := cryptolabs.NewDSS(251, 25, 17, 11, 113), int64(17)
+	if s := signer.Encode(m, 0); !signer.Verify(s, m) {
+		t.Errorf("cryptolabs_test: got unverification for s = %d", s)
+	}
+	signer, m = cryptolabs.NewDSS(251, 125, 20, 7, 21), int64(51)
+	if s := signer.Encode(m, 0); !signer.Verify(s, m) {
+		t.Errorf("cryptolabs_test: got unverification for s = %d", s)
+	}
+	signer = cryptolabs.NewExplicitDSS(271, 27, 106, 28, 7)
+	if !signer.Verify(3, 17) {
+		t.Errorf("cryptolabs_test: got unverification")
+	}
+	signer = cryptolabs.NewExplicitDSS(271, 27, 106, 28, 13)
+	if signer.Verify(8, 17) {
+		t.Errorf("cryptolabs_test: got verification")
+	}
+	signer = cryptolabs.NewExplicitDSS(271, 27, 106, 28, 17)
+	if !signer.Verify(10, 17) {
+		t.Errorf("cryptolabs_test: got unverification")
+	}
 }
